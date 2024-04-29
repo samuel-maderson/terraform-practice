8e3bf0fae25dcd50b3d2d4e487913a41e274a942
= <samuel.maderson@gmail.com>
2024-04-28 21:34:45 -0300

    first commit

---
+++ alb-ec2-autoscaling/main.tf
@@ -0,0 +1,463 @@
+provider "aws" {
+  region = local.region
+}
+
+data "aws_availability_zones" "available" {}
+data "aws_ami" "amazon_linux" {
+  most_recent = true
+  owners      = ["amazon"]
+
+  filter {
+    name   = "name"
+    values = ["al2023-ami-2023*86_64"]
+  }
+}
+
+locals {
+  name   = "${var.project.environment}-${var.project.name}"
+  region = "us-east-1"
+
+  vpc_cidr = var.cidr
+  azs      = slice(data.aws_availability_zones.available.names, 0, 2)
+
+  tags = {
+      example    = local.name
+      GithubRepo = "terraform-aws-vpc"
+      GithubOrg  = "terraform-aws-modules"
+  }
+}
+
+################################################################################
+# VPC Module
+################################################################################
+
+module "vpc" {
+  source  = "terraform-aws-modules/vpc/aws"
+  version = "5.8.1"
+
+  name = local.name
+  cidr = local.vpc_cidr
+
+  azs             = local.azs
+  private_subnets = [for k, v in local.azs : cidrsubnet(local.vpc_cidr, 2, k)]
+  public_subnets = [for k, v in local.azs : cidrsubnet(local.vpc_cidr, 2, k+2)]
+
+  enable_nat_gateway = true
+  single_nat_gateway = true
+
+  tags = local.tags
+}
+
+module "alb" {
+  source  = "terraform-aws-modules/alb/aws"
+  version = "9.9.0"
+
+  name    = local.name
+  vpc_id  = module.vpc.vpc_id
+  subnets = module.vpc.public_subnets
+
+  # For example only
+  enable_deletion_protection = false
+
+  # Security Group
+  security_group_ingress_rules = {
+    all_https = {
+      from_port   = 80
+      to_port     = 80
+      ip_protocol = "tcp"
+      description = "HTTPS web traffic"
+      cidr_ipv4   = "0.0.0.0/0"
+    }
+  }
+  security_group_egress_rules = {
+    all = {
+      ip_protocol = "-1"
+      cidr_ipv4   = module.vpc.vpc_cidr_block
+    }
+  }
+
+  # access_logs = {
+  #   bucket = module.log_bucket.s3_bucket_id
+  #   prefix = "access-logs"
+  # }
+
+  # connection_logs = {
+  #   bucket  = module.log_bucket.s3_bucket_id
+  #   enabled = true
+  #   prefix  = "connection-logs"
+  # }
+
+  listeners = {
+    ex_http = {
+      port     = 80
+      protocol = "HTTP"
+
+      forward = {
+        target_group_key = "ex_asg"
+      }
+    }
+  }
+
+  target_groups = {
+    ex_asg = {
+      backend_protocol                  = "HTTP"
+      backend_port                      = 80
+      target_type                       = "instance"
+      deregistration_delay              = 5
+      load_balancing_cross_zone_enabled = true
+
+      # There's nothing to attach here in this definition.
+      # The attachment happens in the ASG module above
+      create_attachment = false
+    }
+  }
+
+}
+
+module "autoscaling" {
+  source  = "terraform-aws-modules/autoscaling/aws"
+  version = "7.4.1"
+  # insert the 1 required variable here
+
+  # Autoscaling group
+  name            = "complete-${local.name}"
+  use_name_prefix = false
+  instance_name   = local.name
+
+  ignore_desired_capacity_changes = true
+
+  min_size                  = 0
+  max_size                  = 1
+  desired_capacity          = 1
+  wait_for_capacity_timeout = 0
+  default_instance_warmup   = 300
+  health_check_type         = "EC2"
+  vpc_zone_identifier       = module.vpc.private_subnets
+  service_linked_role_arn   = aws_iam_service_linked_role.autoscaling.arn
+
+  # Traffic source attachment
+  create_traffic_source_attachment = true
+  traffic_source_identifier        = module.alb.target_groups["ex_asg"].arn
+  traffic_source_type              = "elbv2"
+
+  initial_lifecycle_hooks = [
+    {
+      name                 = "ExampleStartupLifeCycleHook"
+      default_result       = "CONTINUE"
+      heartbeat_timeout    = 60
+      lifecycle_transition = "autoscaling:EC2_INSTANCE_LAUNCHING"
+      # This could be a rendered data resource
+      notification_metadata = jsonencode({ "hello" = "world" })
+    },
+    {
+      name                 = "ExampleTerminationLifeCycleHook"
+      default_result       = "CONTINUE"
+      heartbeat_timeout    = 180
+      lifecycle_transition = "autoscaling:EC2_INSTANCE_TERMINATING"
+      # This could be a rendered data resource
+      notification_metadata = jsonencode({ "goodbye" = "world" })
+    }
+  ]
+
+  instance_maintenance_policy = {
+    min_healthy_percentage = 100
+    max_healthy_percentage = 110
+  }
+
+  instance_refresh = {
+    strategy = "Rolling"
+    preferences = {
+      checkpoint_delay             = 600
+      checkpoint_percentages       = [35, 70, 100]
+      instance_warmup              = 300
+      min_healthy_percentage       = 50
+      max_healthy_percentage       = 100
+      auto_rollback                = true
+      scale_in_protected_instances = "Refresh"
+      standby_instances            = "Terminate"
+      skip_matching                = false
+    }
+    triggers = ["tag"]
+  }
+
+  # Launch template
+  launch_template_name        = "complete-${local.name}"
+  launch_template_description = "Complete launch template example"
+  update_default_version      = true
+
+  image_id          = data.aws_ami.amazon_linux.id
+  instance_type     = "t3.micro"
+  user_data         = filebase64("scripts/user_data.sh")
+  ebs_optimized     = true
+  enable_monitoring = true
+
+  create_iam_instance_profile = true
+  iam_role_name               = "complete-${local.name}"
+  iam_role_path               = "/ec2/"
+  iam_role_description        = "Complete IAM role example"
+  iam_role_tags = {
+    CustomIamRole = "Yes"
+  }
+  iam_role_policies = {
+    AmazonSSMManagedInstanceCore = "arn:aws:iam::aws:policy/AmazonSSMManagedInstanceCore"
+  }
+
+  # # Security group is set on the ENIs below
+  # security_groups          = [module.asg_sg.security_group_id]
+
+  block_device_mappings = [
+    {
+      # Root volume
+      device_name = "/dev/xvda"
+      no_device   = 0
+      ebs = {
+        delete_on_termination = true
+        encrypted             = true
+        volume_size           = 20
+        volume_type           = "gp2"
+      }
+      }, {
+      device_name = "/dev/sda1"
+      no_device   = 1
+      ebs = {
+        delete_on_termination = true
+        encrypted             = true
+        volume_size           = 30
+        volume_type           = "gp2"
+      }
+    }
+  ]
+
+  capacity_reservation_specification = {
+    capacity_reservation_preference = "open"
+  }
+
+  cpu_options = {
+    core_count       = 1
+    threads_per_core = 1
+  }
+
+  credit_specification = {
+    cpu_credits = "standard"
+  }
+
+  # enclave_options = {
+  #   enabled = true # Cannot enable hibernation and nitro enclaves on same instance nor on T3 instance type
+  # }
+
+  # hibernation_options = {
+  #   configured = true # Root volume must be encrypted & not spot to enable hibernation
+  # }
+
+  instance_market_options = {
+    market_type = "spot"
+  }
+
+  # license_specifications = {
+  #   license_configuration_arn = aws_licensemanager_license_configuration.test.arn
+  # }
+
+  maintenance_options = {
+    auto_recovery = "default"
+  }
+
+  metadata_options = {
+    http_endpoint               = "enabled"
+    http_tokens                 = "required"
+    http_put_response_hop_limit = 32
+    instance_metadata_tags      = "enabled"
+  }
+
+  network_interfaces = [
+    {
+      delete_on_termination = true
+      description           = "eth0"
+      device_index          = 0
+      security_groups       = [module.asg_sg.security_group_id]
+    },
+    {
+      delete_on_termination = true
+      description           = "eth1"
+      device_index          = 1
+      security_groups       = [module.asg_sg.security_group_id]
+    }
+  ]
+
+  placement = {
+    availability_zone = "${local.region}b"
+  }
+
+  tag_specifications = [
+    {
+      resource_type = "instance"
+      tags          = { WhatAmI = "Instance" }
+    },
+    {
+      resource_type = "volume"
+      tags          = merge({ WhatAmI = "Volume" })
+    },
+    {
+      resource_type = "spot-instances-request"
+      tags          = merge({ WhatAmI = "SpotInstanceRequest" })
+    }
+  ]
+
+  tags = local.tags
+
+  # Autoscaling Schedule
+  schedules = {
+    night = {
+      min_size         = 0
+      max_size         = 0
+      desired_capacity = 0
+      recurrence       = "0 18 * * 1-5" # Mon-Fri in the evening
+      time_zone        = "America/Sao_Paulo"
+    }
+
+    morning = {
+      min_size         = 0
+      max_size         = 1
+      desired_capacity = 1
+      recurrence       = "0 7 * * 1-5" # Mon-Fri in the morning
+    }
+
+    go-offline-to-celebrate-new-year = {
+      min_size         = 0
+      max_size         = 0
+      desired_capacity = 0
+      start_time       = "2031-12-31T10:00:00Z" # Should be in the future
+      end_time         = "2032-01-01T16:00:00Z"
+    }
+  }
+  # Target scaling policy schedule based on average CPU load
+  scaling_policies = {
+    avg-cpu-policy-greater-than-50 = {
+      policy_type               = "TargetTrackingScaling"
+      estimated_instance_warmup = 1200
+      target_tracking_configuration = {
+        predefined_metric_specification = {
+          predefined_metric_type = "ASGAverageCPUUtilization"
+        }
+        target_value = 50.0
+      }
+    },
+    predictive-scaling = {
+      policy_type = "PredictiveScaling"
+      predictive_scaling_configuration = {
+        mode                         = "ForecastAndScale"
+        scheduling_buffer_time       = 10
+        max_capacity_breach_behavior = "IncreaseMaxCapacity"
+        max_capacity_buffer          = 10
+        metric_specification = {
+          target_value = 32
+          predefined_scaling_metric_specification = {
+            predefined_metric_type = "ASGAverageCPUUtilization"
+            resource_label         = "testLabel"
+          }
+          predefined_load_metric_specification = {
+            predefined_metric_type = "ASGTotalCPUUtilization"
+            resource_label         = "testLabel"
+          }
+        }
+      }
+    }
+    request-count-per-target = {
+      policy_type               = "TargetTrackingScaling"
+      estimated_instance_warmup = 120
+      target_tracking_configuration = {
+        predefined_metric_specification = {
+          predefined_metric_type = "ALBRequestCountPerTarget"
+          resource_label         = "${module.alb.arn_suffix}/${module.alb.target_groups["ex_asg"].arn_suffix}"
+        }
+        target_value = 800
+      }
+    }
+    scale-out = {
+      name                      = "scale-out"
+      adjustment_type           = "ExactCapacity"
+      policy_type               = "StepScaling"
+      estimated_instance_warmup = 120
+      step_adjustment = [
+        {
+          scaling_adjustment          = 1
+          metric_interval_lower_bound = 0
+          metric_interval_upper_bound = 10
+        },
+        {
+          scaling_adjustment          = 2
+          metric_interval_lower_bound = 10
+        }
+      ]
+    }
+  }
+}
+
+
+module "asg_sg" {
+  source  = "terraform-aws-modules/security-group/aws"
+  version = "~> 5.0"
+
+  name        = local.name
+  description = "A security group"
+  vpc_id      = module.vpc.vpc_id
+
+  computed_ingress_with_source_security_group_id = [
+    {
+      rule                     = "http-80-tcp"
+      source_security_group_id = module.alb.security_group_id
+    }
+  ]
+  number_of_computed_ingress_with_source_security_group_id = 1
+
+  egress_rules = ["all-all"]
+
+  tags = local.tags
+}
+
+resource "aws_iam_service_linked_role" "autoscaling" {
+  aws_service_name = "autoscaling.amazonaws.com"
+  description      = "A service linked role for autoscaling"
+  custom_suffix    = local.name
+
+  # Sometimes good sleep is required to have some IAM resources created before they can be used
+  provisioner "local-exec" {
+    command = "sleep 10"
+  }
+}
+
+
+resource "aws_security_group" "ec2_sg" {
+
+  name        = "example"
+  description = "Allows necessary ports to the ec2 instance"
+  vpc_id      = module.vpc.vpc_id
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = -1
+    to_port     = -1
+    protocol    = "icmp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+}
---
+++ alb-ec2-autoscaling/outputs.tf
@@ -0,0 +1,5 @@
+// gimme an example of output a value 
+
+output "alb_dns" {
+  value = module.alb.dns_name
+}
